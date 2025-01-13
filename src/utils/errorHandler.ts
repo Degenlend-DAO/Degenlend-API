ee08da949e343207648648f5a1bfe3429dccc34b
Kevin Kamto <kamtokevin@gmail.com>
2025-01-12 22:19:08 -0500

    chore: util files

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,4 @@
+export const handleError = (err: any, message: string) => {
+    console.error(`[ERROR] ${message}:`, err);
+  };
+  
---
+++ src/utils/logger.ts
@@ -0,0 +1,4 @@
+export const log = (message: string, details?: any) => {
+    console.log(`[LOG] ${message}`, details || '');
+  };
+  
