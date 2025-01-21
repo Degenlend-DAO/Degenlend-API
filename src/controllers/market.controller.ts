779688bccb8d7f8d9fa538a171b0420fce680dcb
Kevin Kamto <kamtokevin@gmail.com>
2025-01-21 18:22:29 -0500

    refactor: expanded ctoken service with key functions

--- src/controllers/market.controller.ts
+++ src/controllers/market.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { CTokenService } from '../services/ctoken.service';
-import cTokenAbi from '../abis/CToken.json';
+import cTokenAbi from '../abis/CErc20Immutable.json';
 
 const cTokenAddress = process.env.CTOKEN_ADDRESS!;
 const cTokenService = new CTokenService(cTokenAbi, cTokenAddress);
@@ -24,3 +24,24 @@ export const redeem = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Redeem failed', details: err });
   }
 };
+
+
+export const borrow = async (req: Request, res: Response) => {
+  try {
+    const { amount } = req.body;
+    const txHash = await cTokenService.borrow(amount);
+    res.json({ success: true, txHash});
+  } catch (err) {
+    res.status(500).json({ error: 'Borrow failed', details: err });
+  }
+};
+
+export const repayBorrow = async (req: Request, res: Response) => {
+  try {
+    const {amount } = req.body;
+    const txHash = await cTokenService.repayBorrow(amount);
+    res.json({ success: true, txHash});
+    } catch (err) {
+      res.status(500).json({ error: 'Repay failed', details: err });
+    }
+  };
