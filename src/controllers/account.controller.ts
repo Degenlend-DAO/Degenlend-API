1b13d68912de483aa748c86e3d46b6829ed03d1d
Kevin Kamto <kamtokevin@gmail.com>
2025-01-22 14:21:17 -0500

    feat: filling out get route

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -3,13 +3,15 @@ import { ComptrollerService } from '../services/comptroller.service';
 import { CTokenService } from '../services/ctoken.service';
 import comptrollerAbi from '../abis/Comptroller.json';
 import cTokenAbi from '../abis/CErc20Immutable.json';
+import { formatUnits } from 'ethers';
+import { testnet_addresses } from '../utils/constants';
 
 // Initialize ComptrollerService and CTokenService
-const comptrollerAddress = process.env.COMPTROLLER_ADDRESS!;
-const cTokenAddress = process.env.CTOKEN_ADDRESS!;
+const comptrollerAddress = process.env.COMPTROLLER_ADDRESS! || testnet_addresses.comptroller;
+const cTokenAddress = process.env.CTOKEN_ADDRESS! || testnet_addresses.degenWSX;
 
-const comptrollerService = new ComptrollerService(comptrollerAbi, comptrollerAddress);
-const cTokenService = new CTokenService(cTokenAbi, cTokenAddress);
+const comptrollerService = new ComptrollerService(comptrollerAbi.abi, comptrollerAddress);
+const cTokenService = new CTokenService(cTokenAbi.abi, cTokenAddress);
 
 /**
  * Get account liquidity (borrow capacity)
@@ -19,11 +21,13 @@ export const getAccountLiquidity = async (req: Request, res: Response) => {
     const { userAddress } = req.params;
 
     // Query account liquidity from Comptroller
-    const liquidity = await comptrollerService.getAccountLiquidity(userAddress);
+    const liquidityDetails = await comptrollerService.getAccountLiquidity(userAddress);
+
+    const formattedLiquidity = parseFloat(formatUnits(liquidityDetails.liquidity, 18));
 
     res.json({
       success: true,
-      data: liquidity,
+      data: formattedLiquidity,
     });
   } catch (err) {
     console.error(`[ERROR] Failed to fetch account liquidity: ${err}`);
@@ -64,5 +68,77 @@ export const getAccountBalance = async (req: Request, res: Response) => {
 
 
 /**
- * Get Borrow Capacity
+ * Get Supply Balance
  */
+
+export const getSupplyBalance = async (req: Request, res: Response) => {
+  try {
+    const { userAddress } = req.params;
+
+    
+    res.json({
+      success: true,
+      data: {
+        userAddress,
+        supplyBalance: 1
+      }
+    })
+  } catch (error) {
+    
+  }
+};
+
+/**
+ * Get Borrow Balance
+ */
+export const getBorrowBalance = async (req: Request, res: Response) => {
+  try {
+    const { userAddress } = req.params;
+    res.json({
+      success: true,
+      data: {
+        userAddress,
+        borrowBalance: 1
+      }
+    });
+  } catch (error) {
+    
+  }
+};
+
+/**
+ * Get Borrow Limit
+ */
+export const getBorrowLimit = async (req: Request, res: Response) => {
+  try {
+    const { userAddress } = req.params;
+    res.json({
+      success: true,
+      data: {
+        userAddress,
+        borrowLimit: 1
+      }
+    });
+  } catch (error) {
+    
+  }
+};
+
+/**
+ * Get Net APY
+ */
+
+export const getNetApy = async (req: Request, res: Response) => {
+  try {
+    const { userAddress } = req.params;
+    res.json({
+      success: true,
+      data: {
+        userAddress,
+        netApy: 1
+      }
+    });
+  } catch (error) {
+    
+  }
+};
--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAccountLiquidity, getAccountBalance } from '../controllers/account.controller';
+import { getAccountLiquidity, getAccountBalance, getSupplyBalance, getBorrowBalance, getNetApy, getBorrowLimit } from '../controllers/account.controller';
 
 const router = express.Router();
 
@@ -8,5 +8,17 @@ router.get('/liquidity/:userAddress', getAccountLiquidity);
 
 // Route to get cToken balance of a user
 router.get('/balance/:userAddress', getAccountBalance);
+
+// Route to get supply balance
+router.get('/supply/:userAddress', getSupplyBalance);
+
+// Route to get borrow balance
+router.get('/borrow/:userAddress', getBorrowBalance);
+
+// Route to get the net APY
+router.get('/netAPY/:userAddress', getNetApy);
+
+// Route to get borrow limit
+router.get('/borrowLimit/:userAddress', getBorrowLimit);
 
 export default router;
