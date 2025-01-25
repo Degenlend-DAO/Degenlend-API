e3fc7b52f16b3c23d0a23c8b48beca6bb4a4d481
Kevin Kamto <kamtokevin@gmail.com>
2025-01-24 23:06:06 -0500

    feat: working routes for usdc & wsx

---
+++ src/routes/markets/wsx.routes.ts
@@ -0,0 +1,17 @@
+import { getSupplyAPY, getSupplyBalance, getBorrowBalance, getBorrowAPY, mint, borrow, redeem, repayBorrow, approve } from '../../controllers/markets/wsx.controller';
+// TODO: Implement the following routes: isCollateral, liquidity, borrowLimit, borrowLimitUsed
+
+import express from 'express';
+const router = express.Router();
+
+router.get('/supplyAPY', getSupplyAPY);
+router.get('/borrowAPY', getBorrowAPY);
+router.get('/supplyBalance/:userAddress', getSupplyBalance);
+router.get('/borrowBalance/:userAddress', getBorrowBalance);
+router.post('/approve', approve);
+router.post('/mint', mint);
+router.post('/borrow', borrow);
+router.post('/redeem', redeem);
+router.post('/repayBorrow', repayBorrow);
+
+export default router;
