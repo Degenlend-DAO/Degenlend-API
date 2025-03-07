91dd02e408c85a621518204a45ca9a98d2cc317e
Kevin Kamto <kamtokevin@gmail.com>
2025-03-07 10:06:56 -0500

    feat: first iteration of tests for money markets.  next steps: diving into deeper detail of the money market contracts and their interactions

--- tests/money_markets.test.ts
+++ tests/money_markets.test.ts
@@ -1,6 +1,6 @@
 import "mocha";
 import { expect } from "chai";
-import { request } from "supertest";
+import request from "supertest";
 import app from "../src/app";
 
 
@@ -10,10 +10,74 @@ describe("Money Markets Tests", () => {
         const response = await request(app).get("/api/markets");
         expect(response.status).to.equal(404);
     });
-    it("GET /api/markets/wsx/borrowAPY should return 200", async () => {
-        const response = await request(app).get("/api/markets/wsx/borrowAPY");
-        expect(response.status).to.equal(500);
-        expect(response.body).to.have.property("token");
-        expect(response.body).to.have.property("amount");
+
+    //----------------------------  ROUTES -----------------------//
+
+    // tests for the account entrypoint
+    it("GET /api/account should return 404", async () => {
+        //  This is a url that doesn't exist
+        const response = await request(app).get("/api/account");
+        expect(response.status).to.equal(404);
     });
+
+
+    // tests for the market entrypoint
+    it("GET /api/markets should return 404", async () => {
+        //  This is a url that doesn't exist"
+        const response = await request(app).get("/api/markets");
+        expect(response.status).to.equal(404);
+    });
+
+    // tests for the markets entrypoint
+    it("GET /api/markets/usdc should return 404", async () => {
+        // This is a url that doesn't exist
+        const response = await request(app).get("/api/markets/usdc");
+        expect(response.status).to.equal(404);
+    });
+
+    // tests for the markets entrypoint
+    it("GET /api/markets/wsx should return 404", async () => {
+        // This is a url that doesn't exist
+        const response = await request(app).get("/api/markets/wsx");
+        expect(response.status).to.equal(404);
+    });
+
+    // Diving deeper into individual routes
+
+    // USDC: 9 routes
+
+
+    // WSX: 9 routes
+
+    // Token activities
+
+
+
+    //---------------------------- ACCOUNT ROUTES -----------------------//
+
+    // Test RPC url
+    it("GET /api/account/rpc_url should return 200", async () => {
+
+        const response = await request(app).get("/api/account/rpc_url");
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property("rpcUrl");
+
+    });
+
+    // Test network id
+    it("GET /api/account/network_id should return 200", async () => {
+        const response = await request(app).get("/api/account/network_id");
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property("networkId");
+    });
+
+    it("GET /api/account/chain_id should return 200", async () => {
+        const response = await request(app).get("/api/account/chain_id");
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property("chainId");
+    });
+
+    // TODO: account liquidity tests
+
+
 });
