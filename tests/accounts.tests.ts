53592850738a430f4d689d7f95d26bca0c0a5dc8
Kevin Kamto <kamtokevin@gmail.com>
2025-03-22 04:03:28 -0400

    refactor: cleaned up accounts to be testable

--- tests/accounts.tests.ts
+++ tests/accounts.tests.ts
@@ -28,96 +28,118 @@ describe("Account Tests", () => {
     it("GET /api/account should return 404", async () => {
         //  This is a url that doesn't exist
         
-        const response = await request(app).get("/api/account");
-        expect(response.status).to.equal(404);
+        supertest(server).get("/api/account").end((err, res) => {
+            expect(res.status).to.equal(404)
+        })
     });
 
     // Test RPC url
-    it("GET /api/account/rpc_url should return 200", async (done) => {
+    it("GET /api/account/rpc_url should return 200", async () => {
 
-        // const response = await request(app).get("/api/account/rpc_url");
-        supertest(server).get("/api/account/rpc_url").expect(200).end((err, res) => {
-            if (err) return done(err);
-            // expect(res.body).to.have.property("rpcUrl");
-            return done();
+        // supertest(server).get("/api/account/rpc_url");
+        supertest(server).get("/api/account/rpc_url").end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("rpcUrl");
+            
         })
 
 
     });
 
     // Test network id
-    it("GET /api/account/network_id should return 200", async (done: Done) => {
+    it("GET /api/account/network_id should return 200", async () => {
         supertest(server).get("/api/account/network_id").expect(200).end((err, res) => {
-            if (err) return done(err);
             expect(res.status).to.equal(200);
             expect(res.body).to.have.property("networkId");
-            return done();
         })
 
         // {
-        //     expect(response.status).to.equal(200);
-        //     expect(response.body).to.have.property("networkId");
+        //     expect(res.status).to.equal(200);
+        //     expect(res.body).to.have.property("networkId");
         // }
     });
 
     it("GET /api/account/chain_id should return 200", async () => {
-        const response = await request(app).get("/api/account/chain_id");
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("chainId");
+        
+        supertest(server).get("/api/account/chain_id").end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("chainId");
+        });
+
     });
 
     //---------------------------- ACCOUNT LIQUIDITY ROUTES -----------------------//
 
 
     it("GET /api/account/liquidity/:userAddress should return 200", async () => {
-        const response = await request(app).get(`/api/account/liquidity/${userAddress}`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("liquidity");
+
+        supertest(server).get(`/api/account/liquidity/${userAddress}`).end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("liquidity");
+        });
+
     });
 
     it("GET /api/account/balance/:userAddress should return 200", async (done: Done) => {
-            const response = await request(app).get(`/api/account/balance/${userAddress}`);
-            expect(response.status).to.equal(200);
-            expect(response.body).to.have.property("balance");
+
+            supertest(server).get(`/api/account/balance/${userAddress}`).end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property("balance");
+            })
     });
 
     it("GET /api/account/supplyBalance/:userAddress should return 200", async (done: Done) => {
 
-        const response = await request(app).get(`/api/account/supplyBalance/${userAddress}`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("supplyBalance");
+        supertest(server).get(`/api/account/supplyBalance/${userAddress}`).end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("supplyBalance");
+        })
     });
 
     it("GET /api/account/borrowBalance/:userAddress should return 200", async (done: Done) => {
 
-        const response = await request(app).get(`/api/account/borrowBalance/${userAddress}`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("borrowBalance");
+        supertest(server).get(`/api/account/borrowBalance/${userAddress}`).end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("borrowBalance");
+        })
+        
     });
 
     it("GET /api/account/apy/:userAddress should return 200", async (done: Done) => {
-        const response = await request(app).get(`/api/account/apy/${userAddress}`);
-            expect(response.status).to.equal(200);
-            expect(response.body).to.have.property("apy");
+
+        supertest(server).get(`/api/account/apy/${userAddress}`).end((err, res) =>{
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("apy");
+        });
+
     });
 
-    it("GET /api/account/borrowLimit/:userAddress should return 200", async (done: Done) => {
-        const response = await request(app).get(`/api/account/borrowLimit/${userAddress}`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("borrowLimit");
+    it("GET /api/account/borrowLimit/:userAddress should return 200", async () => {
+        
+        supertest(server).get(`/api/account/borrowLimit/${userAddress}`).end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("borrowLimit");
+        });
+
     });
 
     //---------------------------- ACCOUNT ROUTES ACTIVITIES -----------------------//
 
     it("POST /api/account/enterMarket should return 200", async (done: Done) => {
-        const response = await request(app).post(`/api/account/enterMarket`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("message");
+
+        supertest(server).post(`/api/account/enterMarket`).end((err, res) =>{
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("message");
+        });
+
     });
 
     it("POST /api/account/exitMarket should return 200", async (done: Done) => {
-        const response = await request(app).post(`/api/account/exitMarket`);
-        expect(response.status).to.equal(200);
-        expect(response.body).to.have.property("message");
+
+        supertest(server).post(`/api/account/exitMarket`).end((err, res) => {
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property("message");
+        });
+
     });
 });
