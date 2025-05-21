58c72d08475647aea18d17c6d65bcf29eaeb90a9
Kevin Kamto <kamtokevin@gmail.com>
2025-05-21 16:58:35 -0400

    fix: fresh addresses

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 export const testnet_addresses = {
-    "comptroller": '0x937C076C8D9F85291F5cd22E95b1212C0138A609',
+    "comptroller": '0xB078459124e55Eb9F2937c86c0Ec893ff4FF082b',
     "price_oracle": '0x6ca684b4773aF95AB5AE8d0aB7bA078237536DDF',
     "USDC": '0x5147891461a7C81075950f8eE6384e019e39ab98',
     "WSX": '0x2D4e10Ee64CCF407C7F765B363348f7F62D2E06e',
     "degenlendRelayer": "0x7d3Aef7925B0ad621032F87e42b2Af12e5639F76",
     "degenUSDC": '0x05d225eA760bc4E974b0691bFb0Cf026A7D33279', // old: 0xc3eAff959EB22aea6581D7bf52d4d44BdbbDCcD1
     "degenWSX": '0xD2F6594c692985850F85fd1b08d1F9DE0f719Cb5',  // old: 0xf547a2FE994E2207d4d7F13e69B0b2C0aC24cc2f
-    "degenWSX#CErc20Immutable": '0x77273a5B3779e339B990e4BACf0222fc04e93198', // correct decimals
-    "degenUSDC#CErc20Immutable": '0xC8A9b9B44B7DC55351706d94Cd986B053C5f2EaD' // correct decimals
+    "degenWSX#CErc20Immutable": '0x986208977A741f86D16Bda232a321F3d38E916Fc', // correct decimals
+    "degenUSDC#CErc20Immutable": '0x35f1263CE6c06128c4075A1a82050CEB09fa1eB3' // correct decimals
   }
 
 export const DEGEN_TOKEN_DECIMALS = 8;
