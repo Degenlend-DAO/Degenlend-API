82510e2d0f5fae6b516f6aa5efa1e234d41e669c
Kevin Kamto <kamtokevin@gmail.com>
2025-07-16 12:24:41 -0400

    feat: added production / development check

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { application, Application } from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -10,14 +10,26 @@ dotenv.config();
 
 const app: Application = express();
 
+const PROD = process.env.NODE_ENV;
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes
-app.use('/api/intent', intentRoutes);
-app.use('/api/markets', marketRoutes);
-app.use('/api/account', accountRoutes);
 
+if (PROD === 'development') {
+    // Testnet Routes
+    app.use('/api/intent', intentRoutes);
+    app.use('/api/markets', marketRoutes);
+    app.use('/api/account', accountRoutes);
+    
+}
+
+if (PROD === 'production') {
+    // Mainnet routes
+    app.use('/mainnet/intent', intentRoutes);
+    app.use('/mainnet/markets', marketRoutes);
+    app.use('/mainnet/account', accountRoutes);
+}
 export default app;
